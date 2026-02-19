45c151847ee6ad9e56f475970b9701a2a528f082
Mark Madorable <markazjudaya@gmail.com>
2026-02-19 08:36:10 +0800

    running methods from other files using require

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ sayHi('susan');
 sayHi(names.peter);
 sayHi(names.john);
 
-//calling alternative exporting
+//calling alternative exporting for variables
 const data = require('./6-alternative-export');
 console.log(data);
+
+//calling alternative exporting of methods
+//this executes the method defined in 7-mind-grenade when running node app.js
+require('./7-mind-grenade');
