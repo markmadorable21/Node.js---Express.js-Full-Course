da426069e9a1979868e9444d1bf791fa59ea950f
Mark Madorable <markazjudaya@gmail.com>
2026-02-21 00:41:27 +0800

    http streams

--- 17-streams.js
+++ 17-streams.js
@@ -5,10 +5,7 @@ const { createReadStream } = require('fs');
 // highWaterMark - control size
 // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
 // const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-const stream = createReadStream('../content/big.txt', {
-  highWaterMark: 9000,
-  encoding: 'utf8',
-});
+const stream = createReadStream('./content/big.txt');
 
 stream.on('data', (result) => {
   console.log(result);
