04be3f8096a747284fc5bc4ccf98c16c234dbbea
Mark Madorable <markazjudaya@gmail.com>
2026-02-22 14:22:46 +0800

    moved 01 my node.js course into a folder

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-var http = require('http');
-var fs = require('fs');
+// https status code info, visit developer.rozila.org
+const http = require('http');
 
-http
-  .createServer(function (req, res) {
-    // const text = fs.readFileSync('./content/big.txt', 'utf8')
-    // res.end(text)
-    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
-    fileStream.on('open', () => {
-      fileStream.pipe(res);
-    });
-    fileStream.on('error', (err) => {
-      res.end(err);
-    });
-  })
-  .listen(5000);
+const server = http.createServer((req, res) => {
+  // console.log(req.method)
+  const url = req.url;
+  // home page
+  if (url === '/') {
+    console.log(`URL: ${req.url}`);
+    console.log(`Method: ${req.method}`);
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>home page1</h1>');
+    res.end();
+  }
+  // about page
+  else if (url === '/about') {
+    console.log(`URL: ${req.url}`);
+    console.log(`Method: ${req.method}`);
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>about page</h1>');
+    res.end();
+  }
+  // 404
+  else {
+    console.log(`URL: ${req.url}`);
+    console.log(`Method: ${req.method}`);
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>page not found</h1>');
+    res.end();
+  }
+});
+
+server.listen(5000);
