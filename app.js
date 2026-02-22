c9d3f709dfb09e1ecc84b3e361f52edcab7ddb5d
Mark Madorable <markazjudaya@gmail.com>
2026-02-22 17:00:52 +0800

    ran complete website using https

--- app.js
+++ app.js
@@ -1,25 +1,57 @@
 // https status code info, visit developer.rozila.org
 const http = require('http');
+const { readFileSync } = require('fs');
+
+const homePage = readFileSync(
+  './smilga-node-express/02-express-tutorial/navbar-app/index.html',
+);
+const homeStyles = readFileSync(
+  './smilga-node-express/02-express-tutorial/navbar-app/styles.css',
+);
+const homeImage = readFileSync(
+  './smilga-node-express/02-express-tutorial/navbar-app/logo.svg',
+);
+const homeLogic = readFileSync(
+  './smilga-node-express/02-express-tutorial/navbar-app/browser-app.js',
+);
 
 const server = http.createServer((req, res) => {
   // console.log(req.method)
   const url = req.url;
+  console.log(url);
   // home page
   if (url === '/') {
     console.log(`URL: ${req.url}`);
     console.log(`Method: ${req.method}`);
     res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>home page1</h1>');
+    res.write(homePage);
     res.end();
   }
-  // about page
-  else if (url === '/about') {
+  // styles
+  else if (url === '/styles.css') {
     console.log(`URL: ${req.url}`);
     console.log(`Method: ${req.method}`);
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>about page</h1>');
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homeStyles);
     res.end();
   }
+  // image/logo
+  else if (url === '/logo.svg') {
+    console.log(`URL: ${req.url}`);
+    console.log(`Method: ${req.method}`);
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homeImage);
+    res.end();
+  }
+  // logic
+  else if (url === '/browser-app.js') {
+    console.log(`URL: ${req.url}`);
+    console.log(`Method: ${req.method}`);
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(homeLogic);
+    res.end();
+  }
+
   // 404
   else {
     console.log(`URL: ${req.url}`);
